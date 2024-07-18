9390043d53ca172ec5fcde687f582d580532b7e7
Tyler Collingridge <tylercollingridge@gmail.com>
2024-07-18 12:03:20 -0600

    Create header in renderWeatherBlock (view.js)
    - Move layout css and html to layout dir.

--- src/view.js
+++ src/view.js
@@ -16,10 +16,41 @@ export const FORM = (function () {
             try {
                 const weatherReport = await weather.fetchWeather(location);
                 console.log(weatherReport);
+                // await renderWeatherBlock(location, weatherReport);
             } catch {
                 console.error("Error fetching weather data:", error);
             }
         });
+    }
+
+    // generate html and pull textContent from weather array
+    function renderWeatherBlock(location, weatherArr) {
+        const weatherBlock = document.querySelector(".weather-block");
+
+        // header
+        const header = document.createElement("div");
+        header.classList.add("header");
+        weatherBlock.appendChild(header);
+
+        const title = document.createElement("div");
+        title.classList.add("title");
+        header.appendChild(title);
+
+        const h1 = document.createElement("h1");
+        h1.textContent = "7 Day Weather";
+        title.appendChild(h1);
+
+        const locationDiv = document.createElement("div");
+        locationDiv.id = "location";
+        locationDiv.textContent = `- ${location}`;
+        title.appendChild(locationDiv);
+
+        const headerHr = document.createElement("hr");
+        header.appendChild(headerHr);
+
+        // day zero
+
+        // other days
     }
 
     return { init };
